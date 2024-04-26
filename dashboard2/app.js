6accd9bb1e32b6aa916a0ff156443629393aca49
abidkk <dev.abidkk@gmail.com>
2024-04-26 22:54:30 +0500

    chart added

--- dashboard2/app.js
+++ dashboard2/app.js
@@ -1,8 +1,8 @@
-let menu = () => {
-  // alert("Click ")
-  document.getElementsByClassName("sidebar")[0].style.marginLeft = "100vw";
-  document.getElementsByClassName("sidebar")[0].style.width = "100%";
-};
+// let menu = () => {
+//   // alert("Click ")
+//   document.getElementsByClassName("sidebar")[0].style.marginLeft = "100vw";
+//   document.getElementsByClassName("sidebar")[0].style.width = "100%";
+// };
 $(document).ready(function () {
   var sidebarOpen = false;
   $(".menu-btn").click(function () {
@@ -26,3 +26,26 @@ $(document).ready(function () {
 
 let table1 = new DataTable("#myTable");
 let table2 = new DataTable("#records-table");
+
+
+// Charts
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    datasets: [{
+      label: '# of Votes',
+      data: [18, 8, 12, 2, 14, 5, 10, 7, 7, 4, 11, 9],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
